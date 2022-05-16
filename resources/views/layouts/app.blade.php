e43e1bce68f67141ec5a1c87edbccd122901fb9c
devchandant225 <devchandant225@gmail.com>
2022-05-16 11:34:36 +0545

    new sdk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@ $(document).ready(function() {
             fjs.parentNode.insertBefore(js, fjs);
           }(document, 'script', 'facebook-jssdk'));
         </script>
-
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script type="text/javascript" src={{ asset('js/app.js') }}></script>
     <script type="text/javascript" src={{ asset('js/counter.js') }}></script>
